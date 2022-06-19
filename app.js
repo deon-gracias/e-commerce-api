5355dfe4526fb463ab9de537b62e3252d66f141c
deon-gracias <deongracias1@gmail.com>
2022-06-19 15:34:15 +0530

    Connect to mongodb

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/users");
 
+// Connect to database
+mongoose
+  .connect("mongodb://mongodb:27017/e-commerce")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error.bind(console, "MongoDB connection error:"));
+
+// Launch express
 const app = express();
 
 app.use(logger("dev"));
