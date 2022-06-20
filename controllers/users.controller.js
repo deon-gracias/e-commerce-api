1dac68c431d6d07392794521f4782006faa50518
deon-gracias <deongracias1@gmail.com>
2022-06-20 15:19:55 +0530

    Added get user by id

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,10 +3,18 @@ const User = require("../models/userModel");
 
 async function getAllUsers(req, res, next) {
   const users = await User.find({});
+
   return res.status(200).json(users);
 }
 
-async function getUserById(req, res, next) {}
+async function getUserById(req, res, next) {
+  const user = await User.findById(req.params.id);
+
+  if (!user) return next(createError(404, "User id not found"));
+
+  delete user.password;
+  return res.status(201).send(user);
+}
 
 async function deleteUserById(req, res, next) {
   const user = await User.findByIdAndDelete(req.body._id);
