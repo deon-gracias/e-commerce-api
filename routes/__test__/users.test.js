eefee55f75ac41239f52e652981c79f3500a28b9
deon-gracias <deongracias1@gmail.com>
2022-06-20 15:23:29 +0530

    Added user tests for sign up and delete user

---
+++ routes/__test__/users.test.js
@@ -0,0 +1,52 @@
+const request = require("supertest");
+
+const app = require("../../app.js");
+const { connectToDB, disconnectFromDB } = require("../../lib/db");
+const User = require("../../models/userModel.js");
+
+describe("User", () => {
+  beforeAll(async () => {
+    connectToDB();
+  });
+
+  afterAll(async () => {
+    await User.deleteMany({});
+    disconnectFromDB();
+  });
+
+  const user = {
+    name: "deon",
+    email: "deon@mail.com",
+    password: "1234",
+  };
+  let createdUser = {};
+
+  // Checking if server is working
+  it("should sign up", async () => {
+    const res = await request(app)
+      .post("/users/signup")
+      .send(user)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    createdUser = res.body;
+
+    return expect(res.body).toEqual(
+      expect.objectContaining({
+        _id: expect.any(String),
+        name: expect.any(String),
+        email: expect.any(String),
+      })
+    );
+  });
+
+  // Should delete user
+  it("should delete user", async () => {
+    const res = await request(app)
+      .delete("/users/delete")
+      .send(createdUser)
+      .expect("Content-Type", /json/);
+
+    return expect(res.body).toEqual(expect.objectContaining(createdUser));
+  });
+});
