75549057d8f6904e6d16091dc5492b6082f53426
deon-gracias <deongracias1@gmail.com>
2022-06-20 14:50:28 +0530

    Removed error handler

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -11,8 +11,6 @@ exports.getUserById = async function (req, res, next) {};
 exports.deleteUserById = async function (req, res, next) {
   const user = await User.findByIdAndDelete(req.body._id);
 
-  if (user) return res.status(404).json({ message: "User not found" });
-
   return res.status(200).json(user);
 };
 
@@ -25,13 +23,11 @@ exports.signUp = async function (req, res, next) {
     password: req.body.password,
   });
 
-  const createdUser = await user.save().catch((err, user) => {
-    if (err) return next(createError(406, err));
+  const createdUser = await user.save();
 
-    return res.status(201).send({
-      _id: createdUser._id,
-      name: createdUser.name,
-      email: createdUser.email,
-    });
+  return res.status(201).send({
+    _id: createdUser._id,
+    name: createdUser.name,
+    email: createdUser.email,
   });
 };
