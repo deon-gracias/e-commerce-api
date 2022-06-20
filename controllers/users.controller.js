a08fb3eb8ec55750622ec6065a00f1e8870fb9d0
deon-gracias <deongracias1@gmail.com>
2022-06-20 14:53:11 +0530

    Moved exporting to the end of the file

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,22 +1,22 @@
 const createError = require("http-errors");
 const User = require("../models/userModel");
 
-exports.getAllUsers = async function (req, res, next) {
+async function getAllUsers(req, res, next) {
   const users = await User.find({});
   return res.status(200).json(users);
-};
+}
 
-exports.getUserById = async function (req, res, next) {};
+async function getUserById(req, res, next) {}
 
-exports.deleteUserById = async function (req, res, next) {
+async function deleteUserById(req, res, next) {
   const user = await User.findByIdAndDelete(req.body._id);
 
   return res.status(200).json(user);
-};
+}
 
-exports.signIn = async function (req, res, next) {};
+async function signIn(req, res, next) {}
 
-exports.signUp = async function (req, res, next) {
+async function signUp(req, res, next) {
   const user = new User({
     name: req.body.name,
     email: req.body.email,
@@ -30,4 +30,6 @@ exports.signUp = async function (req, res, next) {
     name: createdUser.name,
     email: createdUser.email,
   });
-};
+}
+
+module.exports = { getAllUsers, getUserById, signIn, signUp, deleteUserById };
